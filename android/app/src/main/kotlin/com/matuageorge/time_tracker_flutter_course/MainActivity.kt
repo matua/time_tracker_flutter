849d298b24b00a4f749de12087357ae78139afe2
matua <$9x=4cg^zNNbF&H.UQR_vmN8jSb2U95>
2021-05-23 11:23:59 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/matuageorge/time_tracker_flutter_course/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.matuageorge.time_tracker_flutter_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/sign_in/sign_in_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Time Tracker'),
+        elevation: 2.0,
+      ),
+      body: _buildContent(),
+    );
+  }
+
+  Widget _buildContent() {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+              color: Colors.orange,
+              child: SizedBox(
+                // width: 100.0,
+                height: 100.0,
+              )),
+          SizedBox(
+            height: 8.0,
+          ),
+          Container(
+              color: Colors.red,
+              child: SizedBox(
+                // width: 100.0,
+                height: 100.0,
+              )),
+          SizedBox(
+            height: 8.0,
+          ),
+          Container(
+              color: Colors.purple,
+              child: SizedBox(
+                // width: 100.0,
+                height: 100.0,
+              )),
+        ],
+      ),
+    );
+  }
+}
